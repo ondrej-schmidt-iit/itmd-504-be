978690a51e23b369659f48eeb0162bcc4de25e78
Ondrej Schmidt <ondrej_schmidt@kb.cz>
2024-08-03 05:46:55 +0200

    ITMD-504-002 init itmd-504-be

---
+++ src/main/java/iit/coursera/itmd504be/Itmd504BeApplication.java
@@ -0,0 +1,13 @@
+package iit.coursera.itmd504be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Itmd504BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Itmd504BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iit/coursera/itmd504be/Itmd504BeApplicationTests.java
@@ -0,0 +1,13 @@
+package iit.coursera.itmd504be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Itmd504BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
